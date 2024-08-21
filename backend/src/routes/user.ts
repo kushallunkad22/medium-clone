51e4f956b83b2f97b5f2f49b5126bcbecaa1ea4f
Kushal Lunkad <108121063@nitt.edu>
2024-08-21 13:44:30 +0530

    correcting the /api/v1/user/signup route

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -28,7 +28,8 @@ userRouter.post('/signup', async (c) => {
 		const user = await prisma.user.create({
 			data: {
 				email: body.username,
-				password: body.password
+				password: body.password,
+        name : body.name
 			}
 		});
 	 
